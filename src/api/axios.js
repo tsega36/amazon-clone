614cd8f925412c749dabc30fb2c6e7c2dbd4bff7
tsega <teklewoldtsega@gmail.com>
2025-12-26 01:37:10 +0300

    final updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/clone-a5af3/us-central1/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export { axiosInstance };
+
+// import axios from 'axios';
+
+// const axiosInstance = axios.create({
+//   //baseURL for firebase
+//   // baseURL: 'http://127.0.0.1:5001/clone-a5af3/us-central1/api',
+//   // headers: { 'Content-Type': 'application/json' },
+
+//   //baseURL for deployed version of amazon on render
+//   baseURL: 'https://amazon-api-miwf.onrender.com',
+// });
+
+// export { axiosInstance };
